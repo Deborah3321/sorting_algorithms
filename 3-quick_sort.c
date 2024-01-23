d552bd6d09ce703924bd837741e9a4c780de506f
Deborah3321 <nchabelengdeborah18@gmail.com>
2024-01-22 21:35:30 -0800

    sorting algorithms

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,10 @@
 #include "sort.h"
 
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
+
 /**
  * swap_ints - Swap two integers in an array.
  * @a: The first integer to swap.
@@ -16,7 +21,7 @@ void swap_ints(int *a, int *b)
 
 /**
  * lomuto_partition - Order a subset of an array of integers according to
- *                    the Lomuto partition scheme (last element as pivot).
+ *                    the lomuto partition scheme (last element as pivot).
  * @array: The array of integers.
  * @size: The size of the array.
  * @left: The starting index of the subset to order.
@@ -48,7 +53,7 @@ int lomuto_partition(int *array, size_t size, int left, int right)
 		print_array(array, size);
 	}
 
-	return above;
+	return (above);
 }
 
 /**
